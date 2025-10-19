d0480bf37d5d9ec53d3b259ba2d33455fbeb7d7d
Shaswot Dhungana <27705829-ShaswotDhungana@users.noreply.gitlab.com>
2025-10-19 16:51:28 +0545

    gradle build updated..

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.api.ApkVariantOutputImpl
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -43,17 +45,13 @@ flutter {
     source = "../.."
 }
 
-
 val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
-
-android.applicationVariants.all {
-    val variant = this // 'this' is the ApplicationVariant object
-    variant.outputs.all {
-        val output = this 
-        val abiIdentifier = output.getFilter(com.android.build.OutputFile.ABI)?.identifier
-        val abiCode = abiCodes[abiIdentifier]
-        if (abiCode != null) {
-            output.setVersionCodeOverride(variant.versionCode * 10 + abiCode)
+android.applicationVariants.configureEach {
+    val variant = this
+    variant.outputs.forEach { output ->
+        val abiVersionCode = abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
+        if (abiVersionCode != null) {
+            (output as ApkVariantOutputImpl).versionCodeOverride = variant.versionCode * 10 + abiVersionCode
         }
     }
 }
