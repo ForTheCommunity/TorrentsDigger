e614077e31287aceac1b0524862028325764bab9
Shaswot Dhungana <27705829-ShaswotDhungana@users.noreply.gitlab.com>
2025-12-14 11:33:56 +0545

    added more custom listings

--- lib_torrents_digger/src/sources/customs/custom.rs
+++ lib_torrents_digger/src/sources/customs/custom.rs
@@ -1,7 +1,3 @@
-// this is a custom source
-// that shows latest torrents from
-// multiple sources.
-
 use core::fmt;
 
 use anyhow::Result;
@@ -12,33 +8,67 @@ use crate::{
 
 #[derive(Debug)]
 pub enum Customs {
-    NyaaLatest,
-    SukebeiNyaaLatest,
+    NyaaLatests,
+    NyaaTrendings,
+    SukebeiNyaaLatests,
+    SukebeiNyaaTrendings,
+    KnabenTrendingMovies,
+    KnabenTrendingTvShows,
 }
 
 impl Customs {
-    const ALL_VARIANTS: &'static [Customs] = &[Self::NyaaLatest, Self::SukebeiNyaaLatest];
+    const ALL_VARIANTS: &'static [Customs] = &[
+        Self::NyaaLatests,
+        Self::NyaaTrendings,
+        Self::SukebeiNyaaLatests,
+        Self::SukebeiNyaaTrendings,
+        Self::KnabenTrendingMovies,
+        Self::KnabenTrendingTvShows,
+    ];
 
     pub fn from_index(index: usize) -> Option<&'static Customs> {
         Self::ALL_VARIANTS.get(index)
     }
 
     pub fn all_customs() -> Vec<String> {
-        [Self::NyaaLatest, Self::SukebeiNyaaLatest]
-            .iter()
-            .map(|category| category.to_string())
-            .collect()
+        [
+            Self::NyaaLatests,
+            Self::NyaaTrendings,
+            Self::SukebeiNyaaLatests,
+            Self::SukebeiNyaaTrendings,
+            Self::KnabenTrendingMovies,
+            Self::KnabenTrendingTvShows,
+        ]
+        .iter()
+        .map(|category| category.to_string())
+        .collect()
     }
 
     pub fn fetch_torrents(source: &Customs) -> Result<(Vec<Torrent>, Option<i64>)> {
         match source {
-            Self::NyaaLatest => {
+            Self::NyaaLatests => {
+                let url = "https://nyaa.si";
+                fetch_torrents(url, AllAvailableSources::Nyaa)
+            }
+            Self::NyaaTrendings => {
                 let url = "https://nyaa.si/?s=seeders&o=desc";
                 fetch_torrents(url, AllAvailableSources::Nyaa)
             }
-            Self::SukebeiNyaaLatest => {
+            Self::SukebeiNyaaLatests => {
+                let url = "https://sukebei.nyaa.si";
+                fetch_torrents(url, AllAvailableSources::SukebeiNyaa)
+            }
+            Self::SukebeiNyaaTrendings => {
                 let url = "https://sukebei.nyaa.si/?s=seeders&o=desc";
                 fetch_torrents(url, AllAvailableSources::SukebeiNyaa)
+            }
+            Self::KnabenTrendingMovies => {
+                let url = "https://knaben.org/browse/3000000/1/seeders";
+                fetch_torrents(url, AllAvailableSources::KnabenDatabase)
+            }
+            Self::KnabenTrendingTvShows => {
+                let url = "https://knaben.org/browse/2000000/1/seeders";
+                fetch_torrents(url, AllAvailableSources::KnabenDatabase)
             }
         }
     }
@@ -47,8 +77,12 @@ impl Customs {
 impl fmt::Display for Customs {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::NyaaLatest => write!(f, "Nyaa Trendings"),
-            Self::SukebeiNyaaLatest => write!(f, "Sukebei Nyaa Trendings"),
+            Self::NyaaLatests => write!(f, "Nyaa Latests"),
+            Self::NyaaTrendings => write!(f, "Nyaa Trendings"),
+            Self::SukebeiNyaaLatests => write!(f, "Sukebei Nyaa Latests"),
+            Self::SukebeiNyaaTrendings => write!(f, "Sukebei Nyaa Trendings"),
+            Self::KnabenTrendingMovies => write!(f, "Knaben Trending Movies"),
+            Self::KnabenTrendingTvShows => write!(f, "Knaben Trending Tv Shows"),
         }
     }
 }
