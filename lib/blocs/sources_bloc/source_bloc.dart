a7c6403d24013b00d02d559d910093a7a39f471a
Shaswot Dhungana <27705829-ShaswotDhungana@users.noreply.gitlab.com>
2026-01-07 22:37:25 +0545

    Source Hydration Complete

--- lib/blocs/sources_bloc/source_bloc.dart
+++ lib/blocs/sources_bloc/source_bloc.dart
@@ -1,14 +1,14 @@
-import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:meta/meta.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:torrents_digger/blocs/sources_bloc/get_source_details_data.dart';
 import 'package:torrents_digger/src/rust/api/internals.dart';
 
 part 'source_bloc.freezed.dart';
+part 'source_bloc.g.dart';
 part 'source_events.dart';
 part 'source_state.dart';
 
-class SourceBloc extends Bloc<SourceWidgetEvents, SourceState> {
+class SourceBloc extends HydratedBloc<SourceWidgetEvents, SourceState> {
   SourceBloc() : super(SourceState(sources: [])) {
     on<LoadSources>(_onLoadSources);
     on<SelectSource>(_onSelectSource);
@@ -27,34 +27,84 @@ class SourceBloc extends Bloc<SourceWidgetEvents, SourceState> {
   }
 
   void _onSelectSource(SelectSource event, Emitter<SourceState> emit) {
+    // Checking if this newly selected source is in savedSelectedSourceData Map.
+    SourceStateHydration? savedSelectedSourceData =
+        state.savedSelectedSourceData[event.selectedSource];
     emit(
       state.copyWith(
         selectedSource: event.selectedSource,
-        // resetting category , filter , sorting values when source changes
-        selectedCategory: null,
-        selectedFilter: null,
-        selectedSorting: null,
-        selectedSortingOrder: null,
+        // Hydrating Saved Source Data or resetting to null if none exist
+        selectedCategory: savedSelectedSourceData?.selectedCategory,
+        selectedFilter: savedSelectedSourceData?.selectedFilter,
+        selectedSorting: savedSelectedSourceData?.selectedSorting,
+        selectedSortingOrder: savedSelectedSourceData?.selectedSortingOrder,
       ),
     );
   }
 
   void _onSelectCategory(SelectCategory event, Emitter<SourceState> emit) {
-    emit(state.copyWith(selectedCategory: event.category));
+    emit(
+      _updateSavedSourceData(state.copyWith(selectedCategory: event.category)),
+    );
   }
 
   void _onSelectFilter(SelectFilter event, Emitter<SourceState> emit) {
-    emit(state.copyWith(selectedFilter: event.filter));
+    emit(_updateSavedSourceData(state.copyWith(selectedFilter: event.filter)));
   }
 
   void _onSelectSorting(SelectSorting event, Emitter<SourceState> emit) {
-    emit(state.copyWith(selectedSorting: event.sorting));
+    emit(
+      _updateSavedSourceData(state.copyWith(selectedSorting: event.sorting)),
+    );
   }
 
   void _onSelectSortingOrder(
     SelectSortingOrder event,
     Emitter<SourceState> emit,
   ) {
-    emit(state.copyWith(selectedSortingOrder: event.sortingOder));
+    emit(
+      _updateSavedSourceData(
+        state.copyWith(selectedSortingOrder: event.sortingOder),
+      ),
+    );
   }
+
+  SourceState _updateSavedSourceData(SourceState state) {
+    // if no source is selected, we can't save any data.
+    if (state.selectedSource == null) return state;
+
+    // getting existing data for this source
+    // or create a new blank one if it is new;
+    final currentSavedSourceData =
+        state.savedSelectedSourceData[state.selectedSource!] ??
+        const SourceStateHydration();
+
+    // new config object that combines the old config with the
+    // currently selected values in the state.
+    final newSourceData = currentSavedSourceData.copyWith(
+      selectedCategory: state.selectedCategory,
+      selectedFilter: state.selectedFilter,
+      selectedSorting: state.selectedSorting,
+      selectedSortingOrder: state.selectedSortingOrder,
+    );
+
+    // a copy of the main map so we can modify it.
+    final newSavedSourceData = Map<String, SourceStateHydration>.from(
+      state.savedSelectedSourceData,
+    );
+
+    // updating
+    newSavedSourceData[state.selectedSource!] = newSourceData;
+
+    // Returning the state with the updated map.
+    // Because the state changed, HydratedBloc will automatically save this to Hive Database/Box.
+    return state.copyWith(savedSelectedSourceData: newSavedSourceData);
+  }
+
+  @override
+  SourceState? fromJson(Map<String, dynamic> json) =>
+      SourceState.fromJson(json);
+
+  @override
+  Map<String, dynamic>? toJson(SourceState state) => state.toJson();
 }
--- lib/blocs/sources_bloc/source_state.dart
+++ lib/blocs/sources_bloc/source_state.dart
@@ -5,13 +5,27 @@ abstract class SourceState with _$SourceState {
   const SourceState._();
 
   const factory SourceState({
-    required List<InternalSource> sources,
+    // This annotation tells the app: "Do NOT save the list of sources to the disk".
+    // We want to load fresh source details from the rust-side every time the app starts.
+    // ignore: invalid_annotation_target
+    @JsonKey(includeFromJson: false, includeToJson: false)
+    @Default([])
+    List<InternalSource> sources,
     String? selectedSource,
     String? selectedCategory,
     String? selectedFilter,
     String? selectedSorting,
     String? selectedSortingOrder,
+
+    // This holds the memory of selectedCategory, selectedFilter, selectedSorting,
+    // selectedSortingOrder for ALL sources, not just the active one.
+    @Default({}) Map<String, SourceStateHydration> savedSelectedSourceData,
   }) = _SourceState;
+
+  // This enables the entire state (including the savedSelectedSourceData map)
+  // to be loaded from hive database/box by HydratedBloc.
+  factory SourceState.fromJson(Map<String, dynamic> json) =>
+      _$SourceStateFromJson(json);
 
   InternalSourceDetails? get selectedDetails {
     if (selectedSource == null) {
@@ -25,4 +39,21 @@ abstract class SourceState with _$SourceState {
     }
     return null;
   }
+}
+
+// Source State Hydration
+@freezed
+abstract class SourceStateHydration with _$SourceStateHydration {
+  // defining constructor with fields that we need to hydrate.
+  const factory SourceStateHydration({
+    String? selectedCategory,
+    String? selectedFilter,
+    String? selectedSorting,
+    String? selectedSortingOrder,
+  }) = _SourceStateHydration;
+
+  // This allows this object to be created from
+  // JSON data that is saved in a hive database/box by HydratedBloc.
+  factory SourceStateHydration.fromJson(Map<String, dynamic> json) =>
+      _$SourceStateHydrationFromJson(json);
 }
