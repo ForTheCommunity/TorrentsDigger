9911ba9889c61b4d2d1a13cef2c25cb52eb356f8
Wind xebec <sameerchettri707@gmail.com>
2026-02-07 07:42:01 +0400

    made Scaffold Messenger Theme Specific

--- lib/ui/widgets/scaffold_messenger.dart
+++ lib/ui/widgets/scaffold_messenger.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:torrents_digger/blocs/themes_bloc/themes_bloc.dart';
+import 'package:torrents_digger/themes/light_theme.dart';
 
 // global key for Scaffold Messenger so that it can
 // popup in any screen/widget.
@@ -9,16 +12,28 @@ void createSnackBar({required String message, required int duration}) {
   //  checks whether the ScaffoldMessenger widget
   //  has been mounted in the widget tree and is ready to be used.
   if (scaffoldMessengerKey.currentState != null) {
+    /// accessing the [BuildContext] of the [ScaffoldMessenger] widget
+    final context = scaffoldMessengerKey.currentState!.context;
+
+    /// accessing the [AppTheme] state from the [ThemesBloc]
+    final appColors = context.read<ThemesBloc>().state.maybeWhen(
+          themeState: (appColors, _, _) => appColors,
+          orElse: () => LightTheme(),
+        );
+
     scaffoldMessengerKey.currentState!.showSnackBar(
       SnackBar(
         content: Text(
           message,
-          style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
+          style: TextStyle(color: appColors.scaffoldMessengerMessageColor),
         ),
         duration: Duration(seconds: duration),
         behavior: SnackBarBehavior.fixed,
-        backgroundColor: Color.fromRGBO(1, 1, 1, 1),
-        shape: Border.all(color: Color.fromRGBO(38, 255, 0, 1), width: 2),
+        backgroundColor: appColors.scaffoldMessengerBackgroundColor,
+        shape: Border.all(
+          color: appColors.scaffoldMessengerBorderColor,
+          width: 2,
+        ),
       ),
     );
   }
